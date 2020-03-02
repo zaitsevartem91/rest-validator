b4191df4ba66605469606d039aa89ee574fd3c0a
zaitsev_idealogic <artem.zaitsev@idealogic.lv>
2020-03-02 19:45:26 +0200

    Readme added

--- example/validate.js
+++ example/validate.js
@@ -1,45 +1,7 @@
 const Validator = require("../index");
 
-const data = {
-      // test: "tru",
-      array: [
-          {
-            test: "lalala",
-            obj: [ {
-              last: 123}
-            ]
-          },
-          {
-            test: "lala",
-            obj: {
-              last: 12377
-            }
-          }
-          ],
-      arrays: ["string", 123, { ttt: {}}],
-      number: 123,
-      subObj: {
-        string: "lalala",
-        newObj: { letsTry: "alalal", us: { ii: 'string'} }
-      }
-    };
-
-const schema = {
-      test: "type:boolean, required:false, min:5, max:10",
-      array: {
-          test: "type:string",
-          obj: {
-              last: "type:number, min:3, max:4"
-          }
-      },
-    arrays: ["type:string", "type:string", { ttt: "type:object"}],
-      number: "type:number",
-      subObj: {
-        string: "type:string",
-        newObj: { letsTry: "type:number", us: { ii: 'type:string'} }
-      }
-    };
-
+const data = require("./data.json");
+const schema = require("./schema.json");
 
 (() => {
   const validator = new Validator({data, schema});
