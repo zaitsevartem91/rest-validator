8ad5c73c3bed9cca82aa88b435269ec52290e9ed
kupnu4 <artem.zaitsev@idealogic.dev>
2020-03-12 18:52:04 +0200

    mysql, postgres support. postgres connect and operations, selecting of postgress public schema tables and relations
    TBD:
    - create rest-schema from tables
    - save schemas in files by provided path

--- example/validate.js
+++ example/validate.js
@@ -3,8 +3,21 @@ const Validator = require("../index");
 const data = require("./data.json");
 const schema = require("./schema.json");
 
-(() => {
-  const validator = new Validator({data, schema});
+(async () => {
+  const validator = new Validator();
   console.log("example validation =>>>>>>>>>>>> ", validator.validateSchema());
   console.log("example generation =>>>>>>>>>>>>", validator.generateSchema());
+  console.log("email =>>>>>>>>>>>>>>>>>>>>>>>>>", validator.isEmail('zaitsevartem91@gmail'));
+
+  const client = await validator.connectDb({
+    user: 'swissy',
+    password: 'swissy',
+    database: 'swissy_user_service',
+    driver: 'postgres',
+    host: '167.172.174.98'
+  });
+  const tables = await client.getTables();
+
+  console.log(tables)
+
 })();
