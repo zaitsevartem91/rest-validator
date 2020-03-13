0b36c26b72e6257562a9d9c3e240212a2c28a356
kupnu4 <artem.zaitsev@idealogic.dev>
2020-03-13 17:05:42 +0200

    postgres support, schemas generator

--- example/validate.js
+++ example/validate.js
@@ -1,23 +1,28 @@
 const Validator = require("../index");
-
+const path = require('path');
 const data = require("./data.json");
 const schema = require("./schema.json");
 
 (async () => {
-  const validator = new Validator();
+  const validator = new Validator({ data, schema });
   console.log("example validation =>>>>>>>>>>>> ", validator.validateSchema());
   console.log("example generation =>>>>>>>>>>>>", validator.generateSchema());
-  console.log("email =>>>>>>>>>>>>>>>>>>>>>>>>>", validator.isEmail('zaitsevartem91@gmail'));
+  console.log("email =>>>>>>>>>>>>>>>>>>>>>>>>>", validator.isEmail('myEmail1@gmail'));
+  console.log("regex =>>>>>>>>>>>>>>>>>>>>>>>>>", validator.checkRegex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})$/,
+      'myEmail1@gmail'));
 
+  // database connector
   const client = await validator.connectDb({
-    user: 'swissy',
-    password: 'swissy',
-    database: 'swissy_user_service',
+    user: 'root',
+    password: 'postgres',
+    database: 'postgres',
     driver: 'postgres',
-    host: '167.172.174.98'
+    host: '100.100.100.10', // default 127.0.0.1
+    port: '5422' // default 5432
   });
-  const tables = await client.getTables();
 
-  console.log(tables)
+  // generate validator schemas from current database
+  await client.getTables(path.dirname(require.main.filename || process.mainModule.filename)+'/myData',true);
 
 })();
